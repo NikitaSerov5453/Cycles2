f6923239e893c4ad2a832551fbd53510dc887348
dante5453 <nikita.serov5453@gmail.com>
2023-07-01 19:17:46 +0300

    9 done

--- src/Lesson9.java
+++ src/Lesson9.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lesson9 {
     /**
      * Задание 9
@@ -14,10 +16,29 @@ public class Lesson9 {
      */
 
     public static void main(String[] args) {
-
+        fir(scanner().nextInt(), scanner().nextInt());
     }
 
-    public static void fir() {
+    public static void fir(int weight, int numberOfTiers) {
+        int indent = weight+ numberOfTiers;
+        for (int c = 0; c < numberOfTiers; c++) {
+            for (int i = 0; i < weight; i++) {
+                for (int j = i; j < indent; j++) { //левый отступ
+                    System.out.print(" ");
+                }
+                for (int j = -i; j < 1; j++){ // левая половина
+                    System.out.print("*");
+                }
+                for (int j = -i; j < 0; j++) { // правая половина
+                    System.out.print("*");
+                }
+                System.out.print("\n");
+            }
+            weight +=1;
+        }
+    }
 
+    public static Scanner scanner() {
+        return new Scanner(System.in);
     }
 }
